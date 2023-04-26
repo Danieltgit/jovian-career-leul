fd9cf6f59659aa63a65b4f03caffd622a87775fe
Danieltgit <104619921+Danieltgit@users.noreply.github.com>
2023-04-26 08:52:15 +0000

    Hello World is added

---
+++ main.py
@@ -0,0 +1,17 @@
+""" this is the first 
+python based webased programming 
+thuis is my sample personal website
+
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+  return "Hello there"
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug = True)
